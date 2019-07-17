99fec95558cd69a495090e93a225833b5671906c
Francisca Opoku <franciscaopoku369@gmail.com>
2019-07-17 15:57:58 +0000

    hello! check out

--- my_app.py
+++ my_app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request, redirect
  
 app = Flask(__name__)
-
+items=[]
 @app.route('/')
 def index():
-     return render_template('index.html')
+    return render_template('form.html',items=items)
+    
+@app.route('/add_todo')
+def add_todo():
+    item=request.args.get('item')
+    items.append(item)
+   
+    return redirect("/",)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
