e65412b3382d11931c78a6397d459c4937a999ce
Francisca Opoku <franciscaopoku369@gmail.com>
2019-07-18 11:39:23 +0000

    chech out

--- my_app.py
+++ my_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
- 
+import os
 app = Flask(__name__)
 items=[]
 @app.route('/')
@@ -14,4 +14,5 @@ def add_todo():
     return redirect("/",code=302 )
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0',port=port)
